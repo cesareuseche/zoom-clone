7c19fc62f6e5cc4838d7e8a70c247d2204dd5d6f
cesareuseche <cesareuseche@gmail.com>
2021-01-11 20:27:00 -0400

    finished creating the room ID

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const server = require('http').Server(app)
-const { v4: uuidv4 } = require('uuid')
-
+const { v4: uuidv4 } = require('uuid');
+const server = require('http').Server(app);
 
 app.set('view engine', 'ejs');
 
+
+
 app.get('/', (req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`); //redirects the user to their room ID
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room }); //renders the room
 })
 
 
