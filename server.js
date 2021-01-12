c1b0fa918c3d4d906b1b4415ae7be278d0399dd2
cesareuseche <cesareuseche@gmail.com>
2021-01-11 21:44:46 -0400

    importing peer on the server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 const server = require('http').Server(app);
-const io = require('socket.io')(server)
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require = ('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
 app.use(express.static('public')); //public folder (script file)
 app.set('view engine', 'ejs');
 
