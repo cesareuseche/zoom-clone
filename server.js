ff81b2673bb43cc7811b47f8a0d2b3e8d716f701
cesareuseche <cesareuseche@gmail.com>
2021-01-11 21:11:38 -0400

    importing socket.io

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid');
 const server = require('http').Server(app);
 app.use(express.static('public')); //public folder (script file)
-
 app.set('view engine', 'ejs');
 
 
@@ -16,6 +16,10 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room }); //renders the room
 })
 
-
+io.on('connection', socket => {
+    socket.on('join-room', () = {
+        console.log("joined room");
+    })
+})
 
 server.listen(3030);
