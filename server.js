bcc583236fed0f6f69897532162be8e6192bdb61
cesareuseche <cesareuseche@gmail.com>
2021-01-13 12:40:41 -0400

    recieve and send message functionality

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit('user-connected', userId); //broadcasting that the user it's connecting
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        })
     })
 })
 
